033903334dddd9cb64d5446ef41a379658f898b8
Tyger375 <edoardo.takanen@gmail.com>
2025-01-17 16:42:00 +0000

    refactoring and bootup sequence support started

---
+++ defs.hpp
@@ -0,0 +1,4 @@
+typedef unsigned char Byte;
+typedef unsigned short Word;
+
+typedef unsigned int u32;
---
+++ memory/memory.hpp
@@ -0,0 +1,24 @@
+#include "../defs.hpp"
+
+struct Memory {
+private:
+    static constexpr u32 MAX_MEM = 64 * 1024;
+    Byte Data[MAX_MEM];
+
+public:
+    void init() {
+        for (int i = 0; i < MAX_MEM; ++i) {
+            Data[i] = 1;
+        }
+    }
+
+    Byte operator[](u32 addr) const {
+        return Data[addr];
+    }
+
+    Byte& operator[](u32 addr) {
+        return Data[addr];
+    }
+};
+
+
